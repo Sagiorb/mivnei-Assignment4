b1a1fe9f5228e91af0fe0c194cbde931395624e9
idobin7 <134612185+idobin7@users.noreply.github.com>
2023-05-25 20:26:50 +0300

    Update SkipListExperimentUtils.java

--- SkipListExperimentUtils.java
+++ SkipListExperimentUtils.java
@@ -20,12 +20,62 @@ public class SkipListExperimentUtils {
      *
      * 6. Return the DS and the difference between the times from 3 and 5.
      */
-    public static Pair<AbstractSkipList, Double> measureInsertions(double p, int size) {
-        throw new UnsupportedOperationException("Replace this by your implementation");
+      public static Pair<AbstractSkipList, Double> measureInsertions(double p, int size) {
+        IndexableSkipList sl = new IndexableSkipList(p);
+        int[] arr = chooseRandInsertions(size);
+        double t_s = System.nanoTime();
+        for (int i = 0; i < arr.length; i++) {
+            sl.insert(arr[i]);
+        }
+        double t_f = System.nanoTime();
+        double diff = t_f - t_s;
+        return new Pair<>(sl, diff);
+    }
+
+    private static int[] chooseRandInsertions(int size) {
+        int[] arr = new int[size];
+        int index = 0;
+        while (index < size) {
+            double ranN = Math.ceil(Math.random() * (2 * size));
+            if (ranN % 2 == 0 && !contains(arr, (int) ranN, index)) {
+                arr[index] = (int) ranN;
+                index++;
+            }
+        }
+        return arr;
+    }
+
+    private static boolean contains(int[] arr, int num, int endIndex) {
+        for (int i = 0; i < endIndex; i++) {
+            if (arr[i] == num) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public static double measureSearch(AbstractSkipList skipList, int size) {
-        throw new UnsupportedOperationException("Replace this by your implementation");
+    	int[]arr=chooseRandSerach(size*2);
+    	double t_s=System.nanoTime();
+    	for(int i=0;i<arr.length;i++) {	
+    	    skipList.search(arr[i]); 
+    	}
+    	double t_f=System.nanoTime();
+    	double diff=t_f-t_s;
+    	return diff;
+    }
+    
+    private static int[] chooseRandSerach(int size) {
+        int[] arr = new int[size];
+        int index = 0;
+        while (index < size) {
+            double ranN = Math.ceil(Math.random() * (size));
+            if (!contains(arr, (int) ranN, index)) {
+                arr[index] = (int) ranN;
+                index++;
+            }
+        }
+        return arr;
     }
 
     public static double measureDeletions(AbstractSkipList skipList, int size) {
@@ -107,5 +157,20 @@ public class SkipListExperimentUtils {
         }
         System.out.println("E[ℓ] =" + 1/p);
         System.out.println("");
+    }
+	
+	double[]arrP={0.33, 0.5, 0.75, 0.9};
+    	int[]arrX={1000,2500,5000,10000,15000,20000,50000};   	
+    	for(int i=0;i<arrP.length;i++) {
+    		for(int j=0; j<arrX.length; j++) {
+    			double count=0;
+    			for(int k=0;k<30;k++) {
+    				Pair<AbstractSkipList, Double> pair=measureInsertions(arrP[i], arrX[j]);
+    				count=count+pair.second();
+    			}
+    			count=count/30;	
+    			System.out.println("the average time of insertion with p=" + arrP[i]+ " " + "and x=" +arrX[j]+ " is: " + count);
+    		}	
+    	} 	
     }
 }
