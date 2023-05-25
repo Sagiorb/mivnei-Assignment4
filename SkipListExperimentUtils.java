20cc1a6339620fb93e812469b25a4ec6c3a52c1b
Sagiorb <134610232+Sagiorb@users.noreply.github.com>
2023-05-25 15:47:36 +0300

    SkipListExperimentUtils.java

--- SkipListExperimentUtils.java
+++ SkipListExperimentUtils.java
@@ -1,78 +1,53 @@
-public class SkipListExperimentUtils {
-    public static double measureLevels(double p, int x) {
-    	double count=0;
-    	IndexableSkipList sl=new IndexableSkipList(p);
-    	for(int i=0; i<x; i++) {
-    	  count=count+sl.generateHeight();
-      }
-    	return count/x;
-    }
-
-    /*
-     * The experiment should be performed according to these steps:
-     * 1. Create the empty Data-Structure.
-     * 2. Generate a randomly ordered list (or array) of items to insert.
-     *
-     * 3. Save the start time of the experiment (notice that you should not
-     *    include the previous steps in the time measurement of this experiment).
-     * 4. Perform the insertions according to the list/array from item 2.
-     * 5. Save the end time of the experiment.
-     *
-     * 6. Return the DS and the difference between the times from 3 and 5.
-     */
-    public static Pair<AbstractSkipList, Double> measureInsertions(double p, int size) {
-        throw new UnsupportedOperationException("Replace this by your implementation");
-    }
-
-    public static double measureSearch(AbstractSkipList skipList, int size) {
-        throw new UnsupportedOperationException("Replace this by your implementation");
-    }
-
-    public static double measureDeletions(AbstractSkipList skipList, int size) {
-        throw new UnsupportedOperationException("Replace this by your implementation");
-    }
-
-    public static void main(String[] args) {
         int x=10;
         double p=0.33;
         System.out.println("p=0.33");
+        double[] arr=new double[6];
         for(int j=0;j<4;j++) {
         	System.out.println("x="+x);
 	        for(int i=1;i<=5;i++) {
-	        	System.out.println("l"+ i + "=" + measureLevels(p,x));
+	        	arr[i]=measureLevels(p,x)+1;
+	        	System.out.println("l"+ i + "=" + arr[i]);
 	        }
 	        x*=10;
         }
+        System.out.println("E[ℓ] =" + 1/p);
+        System.out.println("");
         x=10;
         p=0.5;
         System.out.println("p=0.5");
         for(int j=0;j<4;j++) {
         	System.out.println("x="+x);
 	        for(int i=1;i<=5;i++) {
-	        	System.out.println("l"+ i + "=" + measureLevels(p,x));
+	        	arr[i]=measureLevels(p,x)+1;
+	        	System.out.println("l"+ i + "=" + arr[i]);
 	        }
 	        x*=10;
         }
+        System.out.println("E[ℓ] =" + 1/p);
+        System.out.println("");
         System.out.println("p=0.75");
         x=10;
         p=0.75;
         for(int j=0;j<4;j++) {
         	System.out.println("x="+x);
 	        for(int i=1;i<=5;i++) {
-	        	System.out.println("l"+ i + "=" + measureLevels(p,x));
-	        }
+	        	arr[i]=measureLevels(p,x)+1;
+	        	System.out.println("l"+ i + "=" + arr[i]);
+	        	}
 	        x*=10;
         }
+        System.out.println("E[ℓ] =" + 1/p);
+        System.out.println("");
         x=10;
         p=0.9;
         System.out.println("p=0.9");
         for(int j=0;j<4;j++) {
         	System.out.println("x="+x);
 	        for(int i=1;i<=5;i++) {
-	        	System.out.println("l"+ i + "=" + measureLevels(p,x));
+	        	arr[i]=measureLevels(p,x)+1;
+	        	System.out.println("l"+ i + "=" + arr[i]);
 	        }
 	        x*=10;
         }
-        
-    }
-}
+        System.out.println("E[ℓ] =" + 1/p);
+        System.out.println("");
