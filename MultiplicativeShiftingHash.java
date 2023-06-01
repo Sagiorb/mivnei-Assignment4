e5f9533687a3a5b72e2e7430ac2c4d402a77b9a1
idobin7 <134612185+idobin7@users.noreply.github.com>
2023-06-01 22:09:58 +0300

    Update MultiplicativeShiftingHash.java

--- MultiplicativeShiftingHash.java
+++ MultiplicativeShiftingHash.java
@@ -1,24 +1,35 @@
 import java.util.Random;
 
+//import ModularHash.Functor;
+
 public class MultiplicativeShiftingHash implements HashFactory<Long> {
-    public MultiplicativeShiftingHash() {
-        throw new UnsupportedOperationException("Replace this by your implementation");
-    }
+    	private Random random;
+    	
+    	public MultiplicativeShiftingHash() {
+        	this.random = new Random();
+            
+        }
 
     @Override
     public HashFunctor<Long> pickHash(int k) {
-        throw new UnsupportedOperationException("Replace this by your implementation");
+        int a=random.nextInt(Integer.MAX_VALUE - 1) + 2;
+        return new Functor(a, k);
     }
 
     public class Functor implements HashFunctor<Long> {
         final public static long WORD_SIZE = 64;
         final private long a;
         final private long k;
-
+        
+        public Functor(long a, long k) {
+        	this.a=a;
+        	this.k=k;
+        }
         @Override
         public int hash(Long key) {
-            throw new UnsupportedOperationException("Replace this by your implementation");
+            return (int) (a*key >>> (WORD_SIZE-k));
         }
+        
 
         public long a() {
             return a;
