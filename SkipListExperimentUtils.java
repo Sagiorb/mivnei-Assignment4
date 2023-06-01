66becb4c22bb841f55c21b0e1d10c66ec69ddb02
idobin7 <134612185+idobin7@users.noreply.github.com>
2023-06-01 22:09:17 +0300

    Update SkipListExperimentUtils.java

--- SkipListExperimentUtils.java
+++ SkipListExperimentUtils.java
@@ -1,5 +1,3 @@
-import org.w3c.dom.Node;
-
 public class SkipListExperimentUtils {
     public static double measureLevels(double p, int x) {
     	double count=0;
@@ -22,7 +20,7 @@ public class SkipListExperimentUtils {
      *
      * 6. Return the DS and the difference between the times from 3 and 5.
      */
-      public static Pair<AbstractSkipList, Double> measureInsertions(double p, int size) {
+    public static Pair<AbstractSkipList, Double> measureInsertions(double p, int size) {
         IndexableSkipList sl = new IndexableSkipList(p);
         int[] arr = chooseRandInsertions(size);
         double t_s = System.nanoTime();
@@ -100,102 +98,106 @@ public class SkipListExperimentUtils {
     }
 
     public static void main(String[] args) {
-        int x=10;
-        double p=0.33;
-        System.out.println("p=0.33");
-        double[] arr=new double[6];
-        double delta=0;
-        for(int j=0;j<4;j++) {
-        	System.out.println("x="+x);
-	        for(int i=1;i<=5;i++) {
-	        	arr[i]=measureLevels(p,x)+1;//measure the level for each x in specific p
-	        	System.out.println("ℓ"+ i + "=" + arr[i]);
-	        	delta=delta + arr[i]-(1/p);//adding all the ℓ for calculating the delta in the end of each X
-	        }
-	        delta=0.2*delta;
-	        System.out.println("the delta is: " + delta);
-	        delta=0;
-	        x*=10;
-        }
-        System.out.println("E[ℓ] =" + 1/p);
-        
-        System.out.println("");
-        x=10;
-        p=0.5;
-        System.out.println("p=0.5");
-        delta=0;
-        for(int j=0;j<4;j++) {
-        	System.out.println("x="+x);
-	        for(int i=1;i<=5;i++) {
-	        	arr[i]=measureLevels(p,x)+1;//measure the level for each x in specific p
-	        	System.out.println("ℓ"+ i + "=" + arr[i]);
-	        	delta=delta + arr[i]-(1/p);//adding all the ℓ for calculating the delta in the end of each X
-	        }
-	        delta=0.2*delta;
-	        System.out.println("the delta is: " + delta);
-	        delta=0;
-	        x*=10;
-        }
-        System.out.println("E[ℓ] =" + 1/p);
-        System.out.println("");
-        System.out.println("p=0.75");
-        x=10;
-        p=0.75;
-        delta=0;
-        for(int j=0;j<4;j++) {
-        	System.out.println("x="+x);
-	        for(int i=1;i<=5;i++) {
-	        	arr[i]=measureLevels(p,x)+1;//measure the level for each x in specific p
-	        	System.out.println("ℓ"+ i + "=" + arr[i]);
-	        	delta=delta + arr[i]-(1/p);//adding all the ℓ for calculating the delta in the end of each X
-	        }
-	        delta=0.2*delta;
-	        System.out.println("the delta is: " + delta);
-	        delta=0;
-	        x*=10;
-        }
-        System.out.println("E[ℓ] =" + 1/p);
-        System.out.println("");
-        x=10;
-        p=0.9;
-        delta=0;
-        System.out.println("p=0.9");
-        for(int j=0;j<4;j++) {
-        	System.out.println("x="+x);
-	        for(int i=1;i<=5;i++) {
-	        	arr[i]=measureLevels(p,x)+1;//measure the level for each x in specific p
-	        	System.out.println("ℓ"+ i + "=" + arr[i]);
-	        	delta=delta + (arr[i] - (1/p));//adding all the ℓ for calculating the delta in the end of each X
-	        }
-	        delta=0.2*delta;
-	        System.out.println("the delta is: " + delta);
-	        delta=0;
-	        x*=10;
-        }
-        System.out.println("E[ℓ] =" + 1/p);
-        System.out.println("");
-    
-	
-    	double[]arrP={0.33, 0.5, 0.75, 0.9};
-    	int[]arrX={1000,2500,5000,10000,15000,20000,50000};   	
-    	for(int i=0;i<arrP.length;i++) {
-    		for(int j=0; j<arrX.length; j++) {
-    			double countInsert=0;
-    			double countSearch=0;
-    			double countDeletion=0;
-    			for(int k=0;k<30;k++) {
-    				Pair<AbstractSkipList, Double> pair=measureInsertions(arrP[i], arrX[j]);
-    				countInsert+=countInsert+pair.second();
-    				countSearch+=measureSearch(pair.first(), arrX[i]);
-    				countDeletion+=measureDeletions(pair.first(), arrX[j]);
-    			}
-    			countInsert=countInsert/30;
-    			countSearch=countSearch/30;	
-    			countDeletion=countDeletion/30;	
-    			System.out.println("the average time of insertion with p=" + arrP[i]+ " and x=" +arrX[j]+ " is: " + countInsert);
-    			System.out.println("the average time of search with p=" + arrP[i]+ " and x=" +arrX[j]+ " is: " + countSearch);
-    			System.out.println("the average time of deletion with p=" + arrP[i]+ " and x=" +arrX[j]+ " is: " + countDeletion);
-    		}	
-    	}	
-    }
+    	//IndexableSkipList sl=new IndexableSkipList(0.5);
+    	//double hey=measureSearch(sl, 5);
+    	//Pair<AbstractSkipList, Double> pair=measureInsertions(0.5, 2);
+    	//double res=measureSearch(pair.first(), 2);
+    	 int x=10;
+         double p=0.33;
+         System.out.println("p=0.33");
+         double[] arr=new double[6];
+         double delta=0;
+         for(int j=0;j<4;j++) {
+         	System.out.println("x="+x);
+ 	        for(int i=1;i<=5;i++) {
+ 	        	arr[i]=measureLevels(p,x)+1;//measure the level for each x in specific p
+ 	        	System.out.println("ℓ"+ i + "=" + arr[i]);
+ 	        	delta=delta + arr[i]-(1/p);//adding all the ℓ for calculating the delta in the end of each X
+ 	        }
+ 	        delta=0.2*delta;
+ 	        System.out.println("the delta is: " + delta);
+ 	        delta=0;
+ 	        x*=10;
+         }
+         System.out.println("E[ℓ] =" + 1/p);
+         
+         System.out.println("");
+         x=10;
+         p=0.5;
+         System.out.println("p=0.5");
+         delta=0;
+         for(int j=0;j<4;j++) {
+         	System.out.println("x="+x);
+ 	        for(int i=1;i<=5;i++) {
+ 	        	arr[i]=measureLevels(p,x)+1;//measure the level for each x in specific p
+ 	        	System.out.println("ℓ"+ i + "=" + arr[i]);
+ 	        	delta=delta + arr[i]-(1/p);//adding all the ℓ for calculating the delta in the end of each X
+ 	        }
+ 	        delta=0.2*delta;
+ 	        System.out.println("the delta is: " + delta);
+ 	        delta=0;
+ 	        x*=10;
+         }
+         System.out.println("E[ℓ] =" + 1/p);
+         System.out.println("");
+         System.out.println("p=0.75");
+         x=10;
+         p=0.75;
+         delta=0;
+         for(int j=0;j<4;j++) {
+         	System.out.println("x="+x);
+ 	        for(int i=1;i<=5;i++) {
+ 	        	arr[i]=measureLevels(p,x)+1;//measure the level for each x in specific p
+ 	        	System.out.println("ℓ"+ i + "=" + arr[i]);
+ 	        	delta=delta + arr[i]-(1/p);//adding all the ℓ for calculating the delta in the end of each X
+ 	        }
+ 	        delta=0.2*delta;
+ 	        System.out.println("the delta is: " + delta);
+ 	        delta=0;
+ 	        x*=10;
+         }
+         System.out.println("E[ℓ] =" + 1/p);
+         System.out.println("");
+         x=10;
+         p=0.9;
+         delta=0;
+         System.out.println("p=0.9");
+         for(int j=0;j<4;j++) {
+         	System.out.println("x="+x);
+ 	        for(int i=1;i<=5;i++) {
+ 	        	arr[i]=measureLevels(p,x)+1;//measure the level for each x in specific p
+ 	        	System.out.println("ℓ"+ i + "=" + arr[i]);
+ 	        	delta=delta + (arr[i] - (1/p));//adding all the ℓ for calculating the delta in the end of each X
+ 	        }
+ 	        delta=0.2*delta;
+ 	        System.out.println("the delta is: " + delta);
+ 	        delta=0;
+ 	        x*=10;
+         }
+         System.out.println("E[ℓ] =" + 1/p);
+         System.out.println("");
+     
+ 	
+     	double[]arrP={0.33, 0.5, 0.75, 0.9};
+     	int[]arrX={1000,2500,5000,10000,15000,20000,50000};   	
+     	for(int i=0;i<arrP.length;i++) {
+     		for(int j=0; j<arrX.length; j++) {
+     			double countInsert=0;
+     			double countSearch=0;
+     			double countDeletion=0;
+     			for(int k=0;k<30;k++) {
+     				Pair<AbstractSkipList, Double> pair=measureInsertions(arrP[i], arrX[j]);
+     				countInsert+=countInsert+pair.second();
+     				countSearch+=measureSearch(pair.first(), arrX[i]);
+     				countDeletion+=measureDeletions(pair.first(), arrX[j]);
+     			}
+     			countInsert=countInsert/30;
+     			countSearch=countSearch/30;	
+     			countDeletion=countDeletion/30;	
+     			System.out.println("the average time of insertion with p=" + arrP[i]+ " and x=" +arrX[j]+ " is: " + countInsert);
+     			System.out.println("the average time of search with p=" + arrP[i]+ " and x=" +arrX[j]+ " is: " + countSearch);
+     			System.out.println("the average time of deletion with p=" + arrP[i]+ " and x=" +arrX[j]+ " is: " + countDeletion);
+     		}	
+     	}	
+     } 
 }
