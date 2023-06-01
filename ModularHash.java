3441bde85b4960e24d575638d3783d6d58e40f07
idobin7 <134612185+idobin7@users.noreply.github.com>
2023-06-01 22:07:51 +0300

    Update ModularHash.java

--- ModularHash.java
+++ ModularHash.java
@@ -1,13 +1,29 @@
 import java.util.Random;
 
 public class ModularHash implements HashFactory<Integer> {
-    public ModularHash() {
-        throw new UnsupportedOperationException("Replace this by your implementation");
+    private Random random;
+	
+	public ModularHash() {
+    	this.random = new Random();
+        
     }
 
     @Override
-    public HashFunctor<Integer> pickHash(int k) {
-        throw new UnsupportedOperationException("Replace this by your implementation");
+    public HashFunctor<Integer> pickHash(int k) {//implementing carter wegman
+        int m=(int)Math.pow(2, k);
+        int a= random.nextInt(Integer.MAX_VALUE - 1) + 1;
+        int b=random.nextInt(Integer.MAX_VALUE);
+        long minLong = (long)(Integer.MAX_VALUE + 1);
+        long maxLong = Long.MAX_VALUE;
+        HashingUtils hs= new HashingUtils();
+        long p = hs.genLong(minLong, maxLong);
+        boolean found=false;
+        while (!found) {
+        	found=hs.runMillerRabinTest(p, 50);
+        	p = hs.genLong(minLong, maxLong);
+        }
+        return new Functor(a, b, p, m);
+        
     }
 
     public class Functor implements HashFunctor<Integer> {
@@ -15,9 +31,16 @@ public class ModularHash implements HashFactory<Integer> {
         final private int b;
         final private long p;
         final private int m;
+        
+        public Functor(int a, int b, long p, int m) {
+            this.a = a;
+            this.b = b;
+            this.p = p;
+            this.m = m;
+        }
         @Override
         public int hash(Integer key) {
-            throw new UnsupportedOperationException("Replace this by your implementation");
+        	return HashingUtils.mod((int)HashingUtils.mod((long)((a * key) + b), p), m);
         }
 
         public int a() {
