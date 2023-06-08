d7cbcbf06fffd6d822c1f4b376bfdde64d29df39
idobin7 <134612185+idobin7@users.noreply.github.com>
2023-06-08 17:02:51 +0300

    Update HashingExperimentUtils.java

--- HashingExperimentUtils.java
+++ HashingExperimentUtils.java
@@ -1,4 +1,6 @@
 import java.util.Collections; // can be useful
+import java.util.List;
+import java.util.ListIterator;
 
 public class HashingExperimentUtils<K> {
     final private static int k = 16;
@@ -139,7 +141,59 @@ public class HashingExperimentUtils<K> {
 
 
 	public static Pair<Double, Double> measureStringOperations() {
-        throw new UnsupportedOperationException("Replace this by your implementation");
+		hashFactory=new StringHash();
+		ChainedHashTable<String, String> cht = new ChainedHashTable<String, String>((HashFactory<String>) hashFactory, k, 1);
+    	int size=(int)(cht.capacity());
+    	HashingUtils hashingUtils = new HashingUtils();
+    	List<String> list=hashingUtils.genUniqueStrings(size, 10, 20);
+    	ListIterator<String> it = list.listIterator();
+    	double t_s_i = System.nanoTime();
+    	while(it.hasNext()) {
+    		cht.insert(it.next(), "ONE");
+    	}
+    	double t_f_i = System.nanoTime();
+    	double diffI = t_f_i - t_s_i;
+    	String[]arrToSearch=createArrToSearchString(size ,list);
+    	double t_s_s = System.nanoTime();
+    	for(int i=0;i<arrToSearch.length;i++) {
+    		cht.search(arrToSearch[i]);
+    	}
+    	double t_f_s = System.nanoTime();
+    	double diffS = t_f_s - t_s_s;
+        return new Pair<>(diffI, diffS);
+    }
+	
+	private static String[] createArrToSearchString(int size, List<String> list) {
+		String []res = new String[size*2];
+        HashingUtils hashingUtils = new HashingUtils();
+        int index = 0;
+        ListIterator<String> it = list.listIterator();
+        while(it.hasNext()) {
+        	res[index]=it.next();
+        	index++;
+        }
+        while (index < res.length) {
+            List<String> genString = hashingUtils.genUniqueStrings(size, 10, 20);
+            ListIterator<String> it2 = genString.listIterator();
+            while(it2.hasNext()) {
+            	String temp=it2.next();
+            	if (!containsString(res, temp, index)) {
+            		res[index] = temp;
+                	index++;
+            	}
+            }
+        }
+        return res;
+    }
+
+
+   private static boolean containsString(String[] arr, String st, int endIndex) {
+        for (int i = 0; i < endIndex; i++) {
+            if (arr[i] != null && arr[i].equals(st)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public static void main(String[] args) {
@@ -169,6 +223,28 @@ public class HashingExperimentUtils<K> {
         	}
         	System.out.println("The avg insertion time with chained hashing with max load factor of "+rlf[i]+" is: "+(avgInsert/30));
         	System.out.println("The avg search time with chained hashing with max load factor of "+rlf[i]+" is: "+(avgSearch/30));
+        } 
+        
+        double avgInsert1=0;
+        double avgSearch1=0;
+        for(int i=0;i<10;i++) {
+        	Pair<Double, Double> res=new Pair(0, 0);
+        	res=measureLongOperations();
+        	avgInsert1=avgInsert1+res.first();
+        	avgSearch1=avgSearch1+res.second();     
         }
+        System.out.println("The avg insertion time of Long to hash table with max load factor of 1 is: "+(avgInsert1/10));
+        System.out.println("The avg search time of Long in hash table with max load factor of 1 is: "+(avgSearch1/10));
+        
+        double avgInsert2=0;
+        double avgSearch2=0;
+        for(int i=0;i<10;i++) {
+        	Pair<Double, Double> res=new Pair(0, 0);
+        	res=measureStringOperations();
+        	avgInsert2=avgInsert2+res.first();
+        	avgSearch2=avgSearch2+res.second();     
+        }
+        System.out.println("The avg insertion time of Long to hash table with max load factor of 1 is: "+(avgInsert2/10));
+        System.out.println("The avg search time of Long in hash table with max load factor of 1 is: "+(avgSearch2/10));
     }
 }
